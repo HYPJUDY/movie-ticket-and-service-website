2f8f72256a618deb150ab60d0467883d63bf5d4e
SYSU-Linxp <445113634@qq.com>
2017-06-07 20:16:50 +0800

    update ReviesController

--- codes/movie-ticket-and-service-website/src/main/java/com/team/legendary/web/controllers/ReviewsController.java
+++ codes/movie-ticket-and-service-website/src/main/java/com/team/legendary/web/controllers/ReviewsController.java
@@ -1,22 +1,55 @@
 package com.team.legendary.web.controllers;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by HYPJUDY on 2017/5/22.
- */
+import com.team.legendary.persistence.entity.Movie;
+import com.team.legendary.persistence.entity.Order;
+import com.team.legendary.persistence.service.MovieService;
+import com.team.legendary.persistence.service.OrderService;
 
+/**
+ * Created by linxp on 2017/6/7.
+ */
 @Controller
+@RequestMapping(value = "/reviews")
 public class ReviewsController {
 
-    @RequestMapping(value = "/reviews")
-    String reviews() {
-        return  "reviews";
-    }
+   
+    @Autowired
+	private OrderService orderService;
+    @Autowired
+    private MovieService movieService;
+	
+	@ModelAttribute("movie") 
+	public Order getMovie(Integer id) {
+		return orderService.findById(id);
+	}
+	
+    @GetMapping
+    public String getMenu(Model model, HttpSession session)
+    {
+        //model.addAttribute("title", "Movie List");
+        Subject subject = SecurityUtils.getSubject();
+        boolean isAuthc = false;
+        if (subject.isAuthenticated()) {
+            isAuthc = true;
+        }
+        model.addAttribute("isAuthc", isAuthc);
 
-    @RequestMapping(value = "/single")
-    String single() {
-        return  "single";
+        List<Movie> movies = movieService.findAll();
+        model.addAttribute("movies", movies);
+
+        return "reviews";
     }
 }
