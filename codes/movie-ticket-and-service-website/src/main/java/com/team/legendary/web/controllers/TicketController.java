3f5402a7a5519c117c8c65e02d86da3c07c47f3f
SYSU-Linxp <445113634@qq.com>
2017-06-07 20:18:05 +0800

    Add TicketController

--- codes/movie-ticket-and-service-website/src/main/java/com/team/legendary/web/controllers/TicketController.java
+++ codes/movie-ticket-and-service-website/src/main/java/com/team/legendary/web/controllers/TicketController.java
@@ -1,52 +1,47 @@
 package com.team.legendary.web.controllers;
 
+import com.team.legendary.persistence.service.MovieService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
-import org.hibernate.sql.ordering.antlr.GeneratedOrderByFragmentParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.team.legendary.persistence.entity.Movie;
 import com.team.legendary.persistence.entity.Order;
-import com.team.legendary.persistence.service.OrderService;
 
+import org.springframework.ui.Model;
+/**
+ * Created by linxp on 2017-6-7.
+ */
 
 @Controller
-
+@RequestMapping("/ticket/{movieName}")
 public class TicketController {
-	@Autowired
-	private OrderService orderService;
-
-	@RequestMapping(value = "/ticket", method = RequestMethod.POST)
-    public String finishOrderTicket(final ModelMap model, @ModelAttribute("ticket") Order order, final BindingResult bindingResult ) {
-    	model.clear();
-		if (bindingResult.hasErrors()) {
-    		return "redirect:/ticket";
-    	}
-		
-		Subject subject = SecurityUtils.getSubject();
-		order.setCustomerName(subject.getPrincipal().toString());
-		String seating = order.getMovieSeating();
-		//order.setExpenditure(expenditure);
-		System.out.println(order.getMovieName() + order.getMovieSeating());
-        return "ticket";
-    }
 	
-	@RequestMapping("ticket/{orderId}")
-	public String orderDetail(@PathVariable("orderId")String orderId) {
-		return	"ticket";
+	@ModelAttribute("order")
+	public Order getOrder() {
+		return new Order();
 	}
+
+    @Autowired
+    private MovieService movieService;
 	
-	@RequestMapping("/ticket_success")
-	public String ticket_success() {
-        return  "ticket_success";
+    @GetMapping
+    public String getTicket(@PathVariable("movieName") String movieName, Model model) {
+
+        Subject subject = SecurityUtils.getSubject();
+        Boolean vipFlag = false;
+        //VIP通道
+        if (subject.hasRole("CustomerVIP") && subject.isPermitted("Dischange")) {
+            vipFlag = true;
+        }
+        model.addAttribute("vipFlag", vipFlag);
+        model.addAttribute("movieName", movieName);
+        return "ticket";
     }
 }
