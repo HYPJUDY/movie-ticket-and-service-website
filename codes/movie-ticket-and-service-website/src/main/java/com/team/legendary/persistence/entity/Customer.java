cd9cef6c48f0964f89dd2e7eb43b32ad3434dd6a
SYSU-Linxp <445113634@qq.com>
2017-06-08 21:40:46 +0800

    update order

--- codes/movie-ticket-and-service-website/src/main/java/com/team/legendary/persistence/entity/Customer.java
+++ codes/movie-ticket-and-service-website/src/main/java/com/team/legendary/persistence/entity/Customer.java
@@ -16,8 +16,8 @@ public class Customer implements Serializable {
 	private static final long serialVersionUID = 1L;
 
     @Id
-	@Column(name = "customer_name")
-    private String customer_name = null;
+	@Column(name = "cname")
+    private String cname = null;
 	
 	@Column(name="password")
 	private String password = null;
@@ -26,16 +26,16 @@ public class Customer implements Serializable {
         super();
     }
 	
-	public Customer(String customerName, String password) {
-		this.customer_name = customerName;
+	public Customer(String cname, String password) {
+		this.cname = cname;
 		this.password = password;
 	}
 
-    public String getCustomerName() {
-        return this.customer_name;
+    public String getCname() {
+        return this.cname;
     }
-    public void setCustomerName(final String customerName) {
-        this.customer_name = customerName;
+    public void setCname(final String cname) {
+        this.cname = cname;
     }
     
     public String getPassword() {
